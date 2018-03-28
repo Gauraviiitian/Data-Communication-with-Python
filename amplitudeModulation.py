9c39edb57f6d4225e2770c155115d6d4df9a2fd3
Gaurav Misra <gauraviiitian@gmail.com>
2018-03-28 20:56:50 +0530

    Add files via upload

---
+++ amplitudeModulation.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Drawing the graph of Carrier Wave
+def draw_carrier(ac, fc):
+    t = np.arange(0, 10, 0.001)
+    xt = ac*np.sin(2*np.pi*fc*t)
+    plt.title("Carrier Wave")
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.plot(t, xt)
+    plt.show()
+
+#Drawing the graph of Message Wave
+def draw_message(am, fm):
+    t = np.arange(0, 10, 0.001)
+    xt = am*np.sin(2*np.pi*fm*t)
+    plt.title("Message Wave")
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.plot(t, xt)
+    plt.show()
+    return xt
+
+#Drawing Modulated Wave
+def draw_modulated(ac, am, fc, fm):
+    t = np.arange(0,10,0.001)
+    xm = am*np.sin(2*np.pi*fm*t)
+    xt = (xm+ac)*np.sin(2*np.pi*fc*t)
+    plt.title("Amplitude Modulated Wave")
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.plot(t, xt, "r", t, xm, "b")
+    plt.show()
+    
+#taking input from user
+ac, fc = map(float, raw_input("Input the amplitude(cm) and frequency(kHz) of carrier signal: ").split())
+am, fm = map(float, raw_input("Input the amplitude and frequency of message signal: ").split())
+
+draw_carrier(ac, fc)
+xm = draw_message(am, fm)
+draw_modulated(ac, am, fc, fm)
